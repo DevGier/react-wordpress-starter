cc0e54d5bfeaf29b9680d6a3e59cad88956a5342
tim de gier <timdegier@MacBook-Air-de-tim.local>
2023-05-30 22:26:43 +0200

    Fixes for ESLint

--- src/layouts/index.tsx
+++ src/layouts/index.tsx
@@ -1,6 +1,5 @@
 import Header from './header'
 import Footer from './footer'
-import SEO from './seo'
 import Main from './main'
 
-export { Header, Footer, SEO, Main }
+export { Header, Footer, Main }
--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,14 @@ import { Routes } from '@generouted/react-router'
 import '@/styles/main.scss'
 import React from 'react'
 
-const container = document.getElementById('app')!
-const root = ReactDOM.createRoot(container)
+const container = document.getElementById('app')
 
-console.log(
-    'test'
-);
+if (container) {
+    const root = ReactDOM.createRoot(container)
 
-root.render(
-    <React.StrictMode>
-        <Routes />
-    </React.StrictMode>
-)
+    root.render(
+        <React.StrictMode>
+            <Routes />
+        </React.StrictMode>
+    )
+}
